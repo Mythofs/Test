e8579b41b8f8ad6776875622a1f46dfbbd19bb31
Mythofs <bxawesome1@gmail.com>
2026-05-22 22:16:14 -0400

    Started craftingmovement

---
+++ Assets/Scripts/Crafting/CraftingMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace Scripts.Crafting
+{
+    public class CraftingMovement : MonoBehaviour
+    {
+        private PlayerControl control;
+        private Vector2 input;
+        private Action<InputAction.CallbackContext> onCancelInput;
+        private int index = 0;
+        private float delay = 0.1f;
+        private float lastMove = 0;
+        [SerializeField] private TextMeshProUGUI sideItemName;
+        [SerializeField] private Image sideItemSprite;
+        [SerializeField] private List<Image> materialImages;
+        [SerializeField] private List<TextMeshProUGUI> materialTexts;
+        [SerializeField] private List<Image> craftingOptions;
+        private void Awake()
+        {
+            control = new PlayerControl();
+            onCancelInput = ctx =>
+            {
+                input = Vector2.zero;
+            };
+            transform.position = craftingOptions[0].transform.position;
+        }
+        private void OnEnable()
+        {
+            control.Enabled();
+            control.UI.Move.performed += 
+        }
+    }
+}
