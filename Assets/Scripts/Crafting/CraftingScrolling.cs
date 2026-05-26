c33cbb331d0a48e5a49a6bba4665c5b9657dfc26
Mythofs <bxawesome1@gmail.com>
2026-05-25 21:49:47 -0400

    Starting crafting scroll feature

---
+++ Assets/Scripts/Crafting/CraftingScrolling.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Scripts.Crafting
+{
+    public class CraftingScrolling : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect scroll;
+        void Update()
+        {
+            GameObject selected = EventSystem.current.currentSelectedGameObject;
+            if (selected == null)
+                return;
+            RectTransform selectedTransform = selected.GetComponent<RectTransform>();
+            if (selectedTransform == null)
+                return;
+            Canvas.ForceUpdateCanvases();
+            Vector2 position = scroll.viewport.InverseTransformPoint(selectedTransform.position);
+            float viewportHeight = scroll.viewport.rect.height;
+            if (position.y < 0)
+                scroll.content.anchoredPosition -= new Vector2(0, position.y);
+            else if (position.y > viewportHeight)
+                scroll.content.anchoredPosition -= new Vector2(0, position.y - viewportHeight);
+        }
+    }
+}
